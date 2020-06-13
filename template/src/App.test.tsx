deec8c58ae894eaaedb7885fe4a0383517a7e268
Angel Lacret <alacret@gmail.com>
2020-06-12 22:16:54 -0400

    cchangin strategy

--- template/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { App } from './App';
-
-test('renders the text "Cobuild Lab Template"', () => {
-  const { getByText } = render(<App />);
-  const textElement = getByText(/Cobuild Lab Template/i);
-  expect(textElement).toBeInTheDocument();
-});
